c348a2092f209e0a0a3b5512fa736651d95e7e60
rug-levin <levinrug98@gmail.com>
2022-04-28 18:47:08 +0200

    added importance-equalizing loss fcn, not yet revised

--- LossFunctions.py
+++ LossFunctions.py
@@ -1,5 +1,5 @@
 from IMPORTS import *
-
+from RACM_ML_OSTI_config import spcnames, spcnames_imp, spcids
 
 # ozone-focused loss function
 o3 = spcids['O3']
@@ -8,4 +8,14 @@ def MSE_focus_o3(pred, target, focus_factor=20):
     target_o3 = torch.cat((target, target[o3:o3+1]*focus_factor))
     return torch.mean(torch.square(pred_o3 - target_o3))
 
-# soon: importance-equalizing loss function
+# in work: importance-equalizing loss function
+# maybe /max is incomplete, maybe the range has to be adjusted instead of max only
+def MSE_equalizer(pred, target, dat_minmax):
+    names_loc = spcnames if pred.size()==95 else spcnames_imp
+    pred_eq_imp   = pred
+    target_eq_imp = target
+    for iSpc, spc in enumerate(names_loc):
+        pred_eq_imp[iSpc]   *= 100/dat_minmax["conc"][spc][1]
+        target_eq_imp[iSpc] *= 100/dat_minmax["conc"][spc][1]
+    return torch.mean(torch.square(pred_eq_imp - target_eq_imp))
+
